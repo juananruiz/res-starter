cee2a2d4f1c8cabb4c572773b68b467066af674b
Juanan Ruiz <juanan@us.es>
2024-10-14 15:18:59 +0200

    Primeras adaptaciones para US

--- api/controllers/microPosts.js
+++ api/controllers/microPosts.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require("../models");
 const { MicroPost } = db;
 
-// This is a simple example for providing basic CRUD routes for
+// Basic CRUD routes for
 // a resource/model. It provides the following:
 //    GET    /api/micro_posts
 //    POST   /api/micro_posts
